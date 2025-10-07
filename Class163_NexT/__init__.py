d2eac5b20a552e40554f8f9f05229342396015e0
CooooldWind_ <3091868003@qq.com>
2025-10-08 01:19:17 +0800

    0.3.10 我认为应该完全修复了那个问题

--- Class163_NexT/__init__.py
+++ Class163_NexT/__init__.py
@@ -6,7 +6,7 @@ Class163_NexT - 网易云音乐下载API库
 from .models import Music, Playlist, Class163
 from .utils import safe_run, selenium_login, playwright_login
 
-__version__ = "0.3.9"
+__version__ = "0.3.10"
 __all__ = [
     "Music", 
     "Playlist", 
